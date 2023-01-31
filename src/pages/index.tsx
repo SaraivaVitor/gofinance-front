e529e87e3a3e11972f0dc6c800938ca67d5985ae
@gustavo.lima <gustavo.lima@vitta.me>
2023-01-30 21:35:42 -0300

    fix: use home component on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,7 @@
-import Chart from "../components/Chart";
-import Card from "../components/Card";
-
-import { Container, RightSide } from "./styles";
-
-const cardData = [
-  {
-    title: "Saldo a pagar",
-    value: "R$ 120,55",
-  },
-  {
-    title: "Saldo a receber",
-    value: "R$ 120,55",
-  },
-  {
-    title: "Saldo total",
-    value: "R$ 120,55",
-  },
-];
+import HomeComponent from "../components/HomeComponent";
 
 const Home = () => (
-  <Container>
-    <Chart />
-    <RightSide>
-      {cardData.map((card) => (
-        <Card key={card.title} title={card.title} value={card.value} />
-      ))}
-      <button>Download</button>
-    </RightSide>
-  </Container>
+  <HomeComponent />
 );
 
 export default Home;
